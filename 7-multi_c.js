ee790ded2ff4b66a19996a4a1902db349b3d114b
caleb ghanney <ghanneycaleb77@gmail.com>
2025-07-13 21:52:57 +0000

    modified the script that prints x times “C is fun”

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,12 @@
-const count = Number(process.argv[2]);
+const n = Number(process.argv[2]);
 
-if (!Number.isInteger(count)) {
+if (!Number.isInteger(n)) {
+  
   console.log('Missing number of occurrences');
-} else {
-  let output = '';
-  for (let i = 0; i < Math.abs(count); i++) {
-    output += 'C is fun\n';
+} else if (n > 0) {
+  let txt = '';
+  for (let i = 0; i < n; i++) {
+    txt += 'C is fun\n';
   }
-  console.log(output.trimEnd());
+  console.log(txt.trimEnd());
 }
